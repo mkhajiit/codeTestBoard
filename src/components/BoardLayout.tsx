2a8876de7b2d24e8665fb5397c984acabb28c5fe
mkhajiit <mkhajiit15@gmail.com>
2025-02-12 13:57:53 +0900

    잘못된 요소 splice로 걸러내기 구현

--- src/components/BoardLayout.tsx
+++ src/components/BoardLayout.tsx
@@ -1,8 +1,7 @@
 // 컨탠츠 폴더에 있는 컴포넌트를 임포트해서 사용하세요
 import React from 'react';
 import styled from 'styled-components';
-
-import AdvancedBasicOperation from './contents/AdvancedBasicOperation.tsx';
+import Splice from './contents/Splice.tsx';
 
 // 스타일드 컴포넌트
 const Layout = styled.div`
@@ -15,10 +14,11 @@ const Layout = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
 function BoardLayout() {
   return (
     <Layout>
-      <AdvancedBasicOperation />
+      <Splice />
     </Layout>
   );
 }
--- src/components/contents/Splice.tsx
+++ src/components/contents/Splice.tsx
@@ -1,9 +1,42 @@
 // Filter 말고 Splice나 indexOf를 활용해서 주어진 배열에서 조건에 맞지않는 단어를 제거하기
-
-import React from 'react';
+// 결론 filter 쓰는게 훨씬 편하고 원본의 불변성을 유지할 수 있음
+import React, { useState } from 'react';
 
 function Splice() {
-  return <div>Splice</div>;
+  const testCase = [
+    'mind',
+    'enjoy',
+    'require',
+    'admit',
+    'permit',
+    'aim',
+    'give up',
+    'hope',
+    'postpone',
+  ];
+
+  const [input, setInput] = useState<string>('');
+
+  const eraseElement = (input) => {};
+  return (
+    <div>
+      <h1>동명사를 목적어로 가지는 동사가 아닌것을 입력하시오</h1>
+      <h1>
+        [
+        {testCase.map((element) => {
+          if (element !== testCase[testCase.length - 1]) {
+            return element + ', ';
+          } else {
+            return element;
+          }
+        })}
+        ]
+      </h1>
+      <label htmlFor='input'>제거할 단어 </label>
+      <input name='input' value={input} onChange={(event) => setInput(event?.target.value)} />
+      <button onClick={() => eraseElement(input)}>제거</button>
+    </div>
+  );
 }
 
 export default Splice;
