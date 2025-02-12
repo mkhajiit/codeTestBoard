b4d275a11e9c1f2ac7e35ab47fb9647898221d82
mkhajiit <mkhajiit15@gmail.com>
2025-02-12 14:27:54 +0900

    splice를 이용한 배열 요소 제거 구현 완료

--- src/components/contents/Splice.tsx
+++ src/components/contents/Splice.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 
 function Splice() {
-  const testCase = [
+  const MyTestCase = [
     'mind',
     'enjoy',
     'require',
@@ -15,9 +15,27 @@ function Splice() {
     'postpone',
   ];
 
-  const [input, setInput] = useState<string>('');
+  const incorrectCase = ['require', 'permit', 'aim', 'hope'];
 
-  const eraseElement = (input) => {};
+  const [testCase, setTestCase] = useState<string[]>(MyTestCase);
+  const [input, setInput] = useState<string>('');
+  const [incorrect, setInCorrect] = useState<boolean>(false);
+
+  const eraseElement = (input) => {
+    const index = testCase.indexOf(input);
+    // splice()는 기존 배열을 수정한 후, 삭제된 요소를 배열로 반환해서 기존배열임(요소가 변해도 배열의 참조값이 변하지 않음)
+    // 따라서 새로운 배열을 만들어줘야 React의 setState가 작동을 한다.
+    if (index !== -1 && incorrectCase.includes(input)) {
+      setTestCase((prev) => {
+        const newArr = [...prev];
+        newArr.splice(index, 1);
+        return newArr;
+      });
+      setInCorrect(false);
+    } else {
+      setInCorrect(true);
+    }
+  };
   return (
     <div>
       <h1>동명사를 목적어로 가지는 동사가 아닌것을 입력하시오</h1>
@@ -35,6 +53,7 @@ function Splice() {
       <label htmlFor='input'>제거할 단어 </label>
       <input name='input' value={input} onChange={(event) => setInput(event?.target.value)} />
       <button onClick={() => eraseElement(input)}>제거</button>
+      <h1>{incorrect ? '다시 입력해주세요!' : ''}</h1>
     </div>
   );
 }
