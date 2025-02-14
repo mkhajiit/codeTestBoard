4ecbd2668146556d74393052d8988bc7679668ee
mkhajiit <mkhajiit15@gmail.com>
2025-02-14 17:23:31 +0900

    메모장 텍스트 저장 구현시작

--- src/components/contents/SaveImage.tsx
+++ src/components/contents/SaveImage.tsx
@@ -1,3 +1,15 @@
-// 1. 메모장에 입력한 텍스트가 저장되도록한다.
+// 1. 메모장에 입력한 텍스트가 저장되도록한다. (React Quill 텍스트 에디터 라이브러리 사용)
 // 2. 이미지를 파이어베이스에 업로드하는 페이지를 만든다.
 // 3. 업로드한 이미지를 화면에 띄우는 페이지를 만든다.
+
+import React from 'react';
+
+function SaveImage() {
+  return (
+    <div>
+      <h1>메모장에 저장할 텍스트 입력하세요</h1>
+    </div>
+  );
+}
+
+export default SaveImage;
